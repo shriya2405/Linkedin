403e5d4e4f034a5c531e545ce3a42e82849bfcf6
shriya totlod <totlodshriya@iitbhilai.ac.in>
2022-01-22 17:41:24 +0530

    files changed

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
+      <h1>Hello saikiran</h1>
+      <h1>Hello shriyaaaaaaaaa</h1>
       <h1>Hello world</h1>
       <h1>Hello world</h1>
     </div>
